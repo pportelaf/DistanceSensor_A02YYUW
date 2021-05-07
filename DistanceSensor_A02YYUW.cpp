8e9ae541ef0d7a802aa55dee56315b29198b766c
pportelaf <pabloportelafrancos@gmail.com>
2021-05-07 22:45:53 +0200

    Add DistanceSensor_A02YYUW code

---
+++ DistanceSensor_A02YYUW.cpp
@@ -0,0 +1,79 @@
+#include "DistanceSensor_A02YYUW.h"
+
+DistanceSensor_A02YYUW::DistanceSensor_A02YYUW(Stream * stream): DistanceSensor_A02YYUW(stream, MIN_DISTANCE, MAX_DISTANCE) {
+	
+}
+
+DistanceSensor_A02YYUW::DistanceSensor_A02YYUW(Stream * stream, unsigned int minDistance, unsigned int maxDistance) {
+  _stream = stream;
+	_minDistance = minDistance;
+  _maxDistance = maxDistance;
+
+}
+
+DistanceSensor_A02YYUW_MEASSURE_STATUS DistanceSensor_A02YYUW::meassure() {
+  unsigned long startTime = millis();
+  unsigned char data[4] = {};
+  int i = 0;
+  unsigned int meassuredDistance;
+
+  _flushSerialInput();
+
+  _distance = _minDistance;
+
+  while (!_stream->available() && i < SERIAL_AVAILABLE_CHECK_CICLES) {
+    i++;
+    delay(SERIAL_AVAILABLE_CHECK_DELAY);
+  }
+
+  i = 0;
+
+  while(_stream->available() && i < 4) {
+    data[i] = _stream->read();
+
+    i++;
+
+    if (data[0] != SERIAL_HEAD_DATA) {
+      i = 0;
+    }
+  }
+
+  if (i != 4) {
+    return DistanceSensor_A02YYUW_MEASSURE_STATUS_ERROR_SERIAL;
+  }
+
+  if (!_checkSum(data)) {
+  	return DistanceSensor_A02YYUW_MEASSURE_STATUS_ERROR_CHECK_SUM;
+  }
+
+  meassuredDistance = ((data[1] << 8) + data[2]);
+
+  if (meassuredDistance < _minDistance) {
+  	return DistanceSensor_A02YYUW_MEASSURE_STATUS_ERROR_MIN_LIMIT;
+  }
+
+  if (meassuredDistance > _maxDistance) {
+	return DistanceSensor_A02YYUW_MEASSURE_STATUS_ERROR_MAX_LIMIT;
+  }
+
+  _distance = meassuredDistance;
+
+  return DistanceSensor_A02YYUW_MEASSURE_STATUS_OK;
+}
+
+unsigned int DistanceSensor_A02YYUW::getDistance() {
+  return _distance;
+}
+
+
+bool DistanceSensor_A02YYUW::_checkSum(unsigned char data[]) {
+	return ((data[0] + data[1] + data[2])& 0x00FF) == data[3];
+}
+
+void DistanceSensor_A02YYUW::_flushSerialInput() {
+  unsigned long startTime = millis();
+
+  while (_stream->available()) {
+    _stream->read();
+  }
+}
---
+++ DistanceSensor_A02YYUW.h
@@ -0,0 +1,41 @@
+#ifndef DistanceSensor_A02YYUW_H
+#define DistanceSensor_A02YYUW_H
+
+#include <Arduino.h>
+
+#define MIN_DISTANCE 30
+#define MAX_DISTANCE 4500
+
+#define SERIAL_BAUD_RATE 9600
+#define SERIAL_HEAD_DATA 0xff
+
+#define SERIAL_AVAILABLE_CHECK_DELAY 50
+#define SERIAL_AVAILABLE_CHECK_CICLES 10 // SERIAL_AVAILABLE_CHECK_DELAY * SERIAL_AVAILABLE_CHECK_CICLES = 500ms
+
+enum {
+    DistanceSensor_A02YYUW_MEASSURE_STATUS_OK = 0,
+    DistanceSensor_A02YYUW_MEASSURE_STATUS_ERROR_CHECK_SUM = -1,
+    DistanceSensor_A02YYUW_MEASSURE_STATUS_ERROR_MAX_LIMIT = -2,
+    DistanceSensor_A02YYUW_MEASSURE_STATUS_ERROR_MIN_LIMIT = -3,
+    DistanceSensor_A02YYUW_MEASSURE_STATUS_ERROR_SERIAL = -4
+};
+
+typedef int DistanceSensor_A02YYUW_MEASSURE_STATUS;
+
+class DistanceSensor_A02YYUW {
+	public:
+		DistanceSensor_A02YYUW(Stream * _stream);
+		DistanceSensor_A02YYUW(Stream * _stream, unsigned int minDistance, unsigned int maxDistance);
+		DistanceSensor_A02YYUW_MEASSURE_STATUS meassure();
+		unsigned int getDistance();
+
+	private:
+		Stream * _stream;
+		unsigned int _minDistance;
+		unsigned int _maxDistance;
+		unsigned int _distance;
+		bool _checkSum(unsigned char data[]);
+		void _flushSerialInput();
+};
+
+#endif
